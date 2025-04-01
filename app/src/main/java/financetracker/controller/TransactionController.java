3d804f062def082f7c9c26fb6f7c36216732cc40
Flor Stellamans <flor.stellamans@student.howest.be>
2025-04-01 12:52:26 +0200

    total amount works

--- app/src/main/java/financetracker/controller/TransactionController.java
+++ app/src/main/java/financetracker/controller/TransactionController.java
@@ -10,24 +10,30 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class TransactionController {
     private static int transactionIdCounter = 0;
+    private TransactionService transactionService;
     
+    // add Transaction
     @FXML private TextField descriptionField;
     @FXML private TextField amountField;
 
+    // total amount
+    @FXML private Label totalAmountLabel;
+
+    // recent transaction table
     @FXML private TableView<Transaction> transactionTableView;
     @FXML private TableColumn<Transaction, String> descriptionColumn;
     @FXML private TableColumn<Transaction, String> amountColumn;
     @FXML private TableColumn<Transaction, String> categoryColumn;
 
+    // combobox
     @FXML private ComboBox<String> categoryComboBox;
-
-    private TransactionService transactionService;
 
     public void setTransactionService(TransactionService transactionService) {
         this.transactionService = transactionService;
@@ -35,30 +41,10 @@ public class TransactionController {
 
     @FXML
     public void initialize() {
-        // ComboBox
-        ObservableList<String> categories = FXCollections.observableArrayList(
-                Category.SPORT.name().toLowerCase(),
-                Category.FOOD.name().toLowerCase(),
-                Category.CLOTHS.name().toLowerCase(),
-                Category.GOING_OUT.name().toLowerCase(),
-                Category.TRANSPORT.name().toLowerCase(),
-                Category.EXTRA.name().toLowerCase()               
-        );
-        categoryComboBox.setItems(categories);
-        categoryComboBox.getSelectionModel().selectFirst();
-
-        // Recent Transactions Table
-        descriptionColumn.setCellValueFactory(cellData -> 
-            new SimpleStringProperty(cellData.getValue().getDescription())
-        );
-        amountColumn.setCellValueFactory(cellData ->
-            new SimpleStringProperty(String.format("€ %.2f", cellData.getValue().getAmount()))
-        );
-        categoryColumn.setCellValueFactory(cellData -> 
-            new SimpleStringProperty(cellData.getValue().getCategory())
-        );
-
+        setupComboBox();
+        setupRecentTransactions();
     }
+
     @FXML
     private void addTransaction() {
         String description = descriptionField.getText();
@@ -77,5 +63,38 @@ public class TransactionController {
     
         transactionTableView.getItems().add(transaction);
         transactionService.addTransaction(transaction);
+
+        double currentTotal = Double.parseDouble(totalAmountLabel.getText());
+        currentTotal += amount;
+        totalAmountLabel.setText(String.format("%.2f", currentTotal));
+    }
+
+    @FXML
+    private void setupComboBox() {
+        ObservableList<String> categories = FXCollections.observableArrayList(
+                Category.SPORT.name().toLowerCase(),
+                Category.FOOD.name().toLowerCase(),
+                Category.CLOTHS.name().toLowerCase(),
+                Category.GOING_OUT.name().toLowerCase(),
+                Category.TRANSPORT.name().toLowerCase(),
+                Category.EXTRA.name().toLowerCase()               
+        );
+        categoryComboBox.setItems(categories);
+        categoryComboBox.getSelectionModel().selectFirst();
+    }
+
+    @FXML
+    private void setupRecentTransactions() {
+        descriptionColumn.setCellValueFactory(cellData -> 
+        new SimpleStringProperty(cellData.getValue().getDescription())
+        );
+
+        amountColumn.setCellValueFactory(cellData ->
+            new SimpleStringProperty(String.format("€ %.2f", cellData.getValue().getAmount()))
+        );
+
+        categoryColumn.setCellValueFactory(cellData -> 
+            new SimpleStringProperty(cellData.getValue().getCategory())
+        );
     }
 }
